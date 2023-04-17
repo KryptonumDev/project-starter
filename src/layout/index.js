b84457e5505a2bdc0f8c7ea94ca05eb0729cd77f
kryptonumstudio <kryptonumstudio@gmail.com>
2023-04-17 15:02:04 +0200

    add orphan test

--- src/layout/index.js
+++ src/layout/index.js
@@ -1,11 +1,21 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Header from "./header";
 import Footer from "./footer";
 import GlobalStyle from "../styles";
 import Cookies from "./cookie-consent";
 
-export default function Layout({ children }) {
+export default function Layout({ children, location }) {
   const [isCookiesOpened, setCookiesOpened] = useState(false)
+
+  useEffect(() => {
+    const paragraphs = Array.from(document.querySelectorAll('h1, h2, h3, h4, h5, h6, p, li, a, button'));
+    paragraphs.forEach(paragraph =>
+      paragraph.childNodes.forEach(node =>
+        node?.nodeType === Node.TEXT_NODE && (node.textContent = node.textContent.replace(orphansRegex, ` $1\u00A0`))
+      )
+    );
+  }, [location])
+
   return (
     <React.Fragment>
       <Cookies isActive={isCookiesOpened} setIsActive={setCookiesOpened} />
